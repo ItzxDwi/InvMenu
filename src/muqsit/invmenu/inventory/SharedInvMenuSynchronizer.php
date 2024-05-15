c008a0567068ff23b85b8067092c1ce0390a5666
ItzxDwi <107537435+ItzxDwi@users.noreply.github.com>
2024-05-15 11:47:41 +0800

    Update SharedInvMenuSynchronizer.php

--- src/muqsit/invmenu/inventory/SharedInvMenuSynchronizer.php
+++ src/muqsit/invmenu/inventory/SharedInvMenuSynchronizer.php
@@ -9,9 +9,9 @@ use pocketmine\inventory\Inventory;
 
 final class SharedInvMenuSynchronizer{
 
-	readonly private Inventory $inventory;
-	readonly private SharedInventorySynchronizer $synchronizer;
-	readonly private SharedInventoryNotifier $notifier;
+	private Inventory $inventory;
+	private SharedInventorySynchronizer $synchronizer;
+	private SharedInventoryNotifier $notifier;
 
 	public function __construct(InvMenu $menu, Inventory $inventory){
 		$this->inventory = $inventory;
